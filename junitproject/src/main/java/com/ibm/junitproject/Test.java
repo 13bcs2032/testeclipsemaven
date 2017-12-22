2664cb83b5819312e5c0277611151ff15b247922
Jovit J <jovitjesus@gmail.com>
2017-12-22 15:44:01 +0530

    Initial Message

---
+++ junitproject/src/main/java/com/ibm/junitproject/Test.java
@@ -0,0 +1,11 @@
+package com.ibm.junitproject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HAI Hello");
+
+	}
+
+}
